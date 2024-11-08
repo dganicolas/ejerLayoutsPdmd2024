cb8c85db2c4b5d73348ea9fd49bb645b1f3e83c8
dganicolas <dganicolas@rafaelalberti.es>
2024-11-08 05:07:18 +0100

    terminado

--- app/src/main/java/com/example/primercontactov2/MainActivity.kt
+++ app/src/main/java/com/example/primercontactov2/MainActivity.kt
@@ -10,9 +10,12 @@ import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
-    var estado = true
+    private var estado = true
+
+    var color: Int = R.color.black
     @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -20,24 +23,27 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val btnEnviar = findViewById<Button>(R.id.btnEnviar)
         val btnCambiarColor = findViewById<Button>(R.id.btnCambiarColor)
-
         btnEnviar.setOnClickListener{
             val textoEdit = findViewById<EditText>(R.id.textoEdit)
             val textito = textoEdit.text.toString()
             if (textito.isNotEmpty()){
                 val intento = Intent(this, MainActivity2::class.java)
+                intento.putExtra("EXTRA_COLOR",color)
                 intento.putExtra("EXTRA_NAME",textito)
                 startActivity(intento)
             }
         }
 
+        //con esta parte la tarea esta completa
         btnCambiarColor.setOnClickListener{
             val mainLayout = findViewById<ConstraintLayout>(R.id.main)
-            if(estado){
-                mainLayout.setBackgroundColor(R.color.white)
+            if(!estado){
+                color= R.color.white
+                mainLayout.setBackgroundColor(ContextCompat.getColor(this,color))
                 estado = false
             }else{
-                mainLayout.setBackgroundColor(R.color.black)
+                color =R.color.black
+                mainLayout.setBackgroundColor(ContextCompat.getColor(this,color))
                 estado = true
             }
 
--- app/src/main/java/com/example/primercontactov2/MainActivity2.kt
+++ app/src/main/java/com/example/primercontactov2/MainActivity2.kt
@@ -1,21 +1,24 @@
     package com.example.primercontactov2
 
-    import android.annotation.SuppressLint
-    import android.content.Intent
     import android.os.Bundle
-    import androidx.activity.enableEdgeToEdge
+    import android.util.Log
     import androidx.appcompat.app.AppCompatActivity
     import androidx.appcompat.widget.AppCompatTextView
-    import androidx.core.view.ViewCompat
-    import androidx.core.view.WindowInsetsCompat
+    import androidx.constraintlayout.widget.ConstraintLayout
+    import androidx.core.content.ContextCompat
 
     class MainActivity2 : AppCompatActivity() {
         override fun onCreate(savedInstanceState: Bundle?) {
             super.onCreate(savedInstanceState)
             setContentView(R.layout.activity_main2)
+            val view2 = findViewById<ConstraintLayout>(R.id.view2)
             val textoRes = findViewById<AppCompatTextView>(R.id.textoFinal)
             var nombre = intent?.extras?.getString("EXTRA_NAME").orEmpty()
+            var color = intent?.extras?.getInt("EXTRA_COLOR")
+            Log.i("XD", color.toString())
+            if(color != null){
+                view2.setBackgroundColor(ContextCompat.getColor(this,color))
+            }
             textoRes.setText("hola $nombre")
-
         }
     }
